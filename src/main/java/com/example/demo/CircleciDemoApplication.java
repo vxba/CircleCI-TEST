af3b507040052e7bdfc41f52437d68b437d77117
An Van <vxba@Ans-MacBook-Pro.local>
2019-04-08 08:57:27 +0700

    update source

--- src/main/java/com/example/demo/CircleciDemoApplication.java
+++ src/main/java/com/example/demo/CircleciDemoApplication.java
@@ -2,12 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CircleciDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CircleciDemoApplication.class, args);
 	}
-
+	
+	@RequestMapping(value="/")
+	public String index() {
+		return "Welcom!";
+	}
 }
---
+++ src/main/java/com/example/demo/controller/MyController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+	
+	@RequestMapping(value = "/hello/{name}")
+	public String hello(@PathVariable("name") String name) {
+		return "Hello " + name;
+	}
+
+}
